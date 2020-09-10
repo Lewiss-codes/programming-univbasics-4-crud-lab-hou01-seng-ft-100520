9e5e7c6204e51e8aa6d335ce7c2e2bffec0ed69c
Luis Penaloza <ldp231@yahoo.com>
2020-09-10 00:39:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -49,6 +49,5 @@ end
 
 def update_element_from_index(array, index_number, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.index("really")
-  array.index("totally")
+  array[3] = "totally"
 end
