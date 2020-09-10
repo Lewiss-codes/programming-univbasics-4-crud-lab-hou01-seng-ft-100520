cbed9c5735d78af227eb33a6e04134392d4f9b7a
Luis Penaloza <ldp231@yahoo.com>
2020-09-10 00:18:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,10 +26,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  animals = ["dog", "cat", "fish", "rabbit", "arrays!"]
-  first_animal = animals.shift 
-  p animals
-  p first_animal
+  
 end
 
 def retrieve_element_from_index(array, index_number)
