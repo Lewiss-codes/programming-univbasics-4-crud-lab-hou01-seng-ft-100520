c02a70181beb3bebfa652b575b0156ec00c97828
Luis Penaloza <ldp231@yahoo.com>
2020-09-10 00:06:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   animals = ["dog", "cat", "fish", "rabbit"]
-  animals << "array" "element"
+  animals.push ("array" "element")
   p animals
 end
 
